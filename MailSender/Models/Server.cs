b61752ff77a2bc2fbffc6dc6654eeb654030f335
PetrPleskach <Pleskach_petr@mail.ru>
2021-02-19 13:53:05 +0300

    Выполнена привязка к тестовым данным

--- MailSender/Models/Server.cs
+++ MailSender/Models/Server.cs
@@ -4,7 +4,7 @@
     {
         public string Name { get; set; }
         public string Adress { get; set; }
-        public int Port { get; set; }
+        public int Port { get; set; } = 25;
         public string Login { get; set; }
         public string Password { get; set; }
         public bool useSSL { get; set; } = true;
