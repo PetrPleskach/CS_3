a8540dc88b428c47a79b620bb83c224007e22b1c
PetrPleskach <Pleskach_petr@mail.ru>
2021-02-25 12:27:56 +0300

    Добавлена инфраструктура для реализации IoC и Dependency Injection

--- MailSender/App.xaml.cs
+++ MailSender/App.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using MailSender.ViewModels;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +17,20 @@ namespace MailSender
     /// </summary>
     public partial class App : Application
     {
+        private static IHost __Hosting;
+
+        public static IHost Hosting => __Hosting ??= CreateHostBuilder(Environment.GetCommandLineArgs()).Build();
+
+        public static IServiceProvider Services => Hosting.Services;
+
+        private static IHostBuilder CreateHostBuilder(string[] args) => Host.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration(c => c.AddJsonFile("appsettings.json", false, false)).ConfigureServices(ServicesConfiguration);
+
+        private static void ServicesConfiguration(HostBuilderContext host, IServiceCollection services)
+        {
+            services.AddSingleton<MainWindowViewModel>();
+
+            
+        }
     }
 }
--- MailSender/ViewModels/MainWindowViewModel.cs
+++ MailSender/ViewModels/MainWindowViewModel.cs
@@ -4,19 +4,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace MailSender.ViewModels
 {
     class MainWindowViewModel : ViewModel
     {
-        private string _title;
+        private string _title = "Рассыльщик";
 
         public string Title
         {
             get { return _title; }
             set => Set(ref _title, value);
         }
-
-
     }
 }
